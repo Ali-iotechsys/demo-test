93a94ebfffb13e853742f0d15fac88612a5be9de
Ali Almohammad <ali@iotechsys.com>
2021-02-21 15:18:13 +0000

    Added Group Tests

--- demo/demo_test.go
+++ demo/demo_test.go
@@ -5,20 +5,18 @@ import (
 	"github.com/go-resty/resty/v2"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"os"
+	"net/http"
 )
 
 var _ = Describe("Demo Test", func() {
 
-	// Create a Resty Client
+	// Create an API Client
 	client := resty.New()
 
-	// Test connection
-	_, err := client.R().Get("")
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
+	// Init authentication token
+	authResult := struct {
+		Jwt string `json:"jwt"`
+	}{Jwt: ""}
 
 	BeforeEach(func() {
 		fmt.Println("this is start test")
@@ -28,22 +26,77 @@ var _ = Describe("Demo Test", func() {
 		fmt.Println("this is end test")
 	})
 
-	Context("when sum are positive", func() {
+	Context("Test Connectivity Feature", func() {
 
-		It("adds two numbers", func() {
-			sum, err := Add(2, 3)
+		It("EB Controller API is reachable", func() {
+			resp, err := client.R().Get("http://192.168.0.7:8085/api/")
 			Expect(err).NotTo(HaveOccurred())
-			Expect(sum).To(Equal(5))
+			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+		})
+
+		It("EB Controller can authenticate a user", func() {
+			resp, err := client.R().
+				SetHeader("accept", "application/json").
+				SetHeader("Content-Type", "application/json").
+				SetBody(`{"Username": "iotech",  "Password": "EdgeBuilder123"}`).
+				SetResult(&authResult).
+				Post("http://192.168.0.7:8085/api/auth")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+			Expect(authResult.Jwt).NotTo(BeEmpty())
 		})
 
 	})
 
-	Context("when sum is negative", func() {
+	Context("Test Groups Feature", func() {
 
-		It("returns an err", func() {
-			_, err := Add(-1, -1)
-			Expect(err).To(HaveOccurred())
+		It("EB Controller API can add a group", func() {
+			resp, err := client.R().
+				SetHeader("accept", "application/json").
+				SetHeader("Content-Type", "application/json").
+				SetHeader("Authorization", authResult.Jwt).
+				SetBody(`[{"Name":"Group1","Description":"This is Group 1"}]`).
+				Post("http://192.168.0.7:8085/api/groups")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
 		})
+
+		It("EB Controller API can view all groups", func() {
+			resp, err := client.R().
+				SetHeader("Content-Type", "application/json").
+				SetHeader("Authorization", authResult.Jwt).
+				Get("http://192.168.0.7:8085/api/groups")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+		})
+
+		It("EB Controller API can view a group by Name", func() {
+			resp, err := client.R().
+				SetQueryParams(map[string]string{
+					"group": "Group1",
+				}).
+				SetHeader("Content-Type", "application/json").
+				SetHeader("Authorization", authResult.Jwt).
+				Get("http://192.168.0.7:8085/api/groups")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+		})
+
+		It("EB Controller API can remove a group", func() {
+			deleteResult := struct {
+				Deleted int `json:"Deleted"`
+			}{Deleted: 0}
+			resp, err := client.R().
+				SetHeader("Content-Type", "application/json").
+				SetHeader("Authorization", authResult.Jwt).
+				SetBody(`["Group1"]`).
+				SetResult(&deleteResult).
+				Delete("http://192.168.0.7:8085/api/groups")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode()).To(Equal(http.StatusOK))
+			Expect(deleteResult.Deleted).To(Equal(1))
+		})
+
 	})
 
 })
